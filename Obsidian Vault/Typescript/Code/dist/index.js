ae1e6e6ab58c888729b50b5b4a4e50585e56c376
Asininite <asininite@gmail.com>
2026-07-24 23:46:28 +0530

    vault backup: 2026-07-24 23:46:28

---
+++ Obsidian Vault/Typescript/Code/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(Math.floor(11.01));
+let numb1 = 12;
+let name2 = "ashwin";
+console.log(numb1);
+console.log(name2);
+//# sourceMappingURL=index.js.map
